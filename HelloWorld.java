c0d00524f7d10a25c341279afd1f1f8aa29fb20f
Erika Earley <erika.earley@pcc.edu>
2020-10-18 22:16:55 -0700

    Added main method that prints hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,13 @@
 package pcc.edu.cis.helloworld;
 
+/**
+ * Created by Erika Earley
+ * 10/18/2020
+ */
+
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
 }
